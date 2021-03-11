24204a94aa4694e889d111c83f94f8a5a6e1093e
Sagun Shrestha <goodsamaritan.4846@gmail.com>
2021-03-10 20:32:30 -0800

    use get command to read comments made by user from a json file

--- getVsPost/index.js
+++ getVsPost/index.js
@@ -1,15 +1,36 @@
 const express = require("express");
 const app = express();
-// app.set("view engine", "ejs");
+const path = require("path");
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
+// const bodyParser = require("body-parser");
+// const multer = require('multer')
+// const upload = multer()
+// app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const words = [
+  { username: "gdawg", comment: "lol hey this is me" },
+  { username: "dips", comment: "hey this is Dips" },
+  { username: "shree", comment: "hey yea yea!" },
+];
+
+app.get("/comments", (req, res) => {
+  res.render("index", { info: words });
+});
 
 app.get("/tacos", (req, res) => {
   //   const { tacos } = req.params;
   //   res.render("bell", { info: tacos });
+
   res.send("tacos get request");
 });
 
-app.post("/burrito", (req, res) => {
-  res.send("burrito post request here");
+app.post("/cars", (req, res) => {
+  const { color, car } = req.body;
+
+  res.send(`your color is ${color} and your car is ${car}`);
 });
 app.listen(3000, (req, res) => {
   console.log("hi");
