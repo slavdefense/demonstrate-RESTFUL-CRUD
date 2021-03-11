eeb22524a6c6aa74c542cc1dde843b3037fe6307
Sagun Shrestha <goodsamaritan.4846@gmail.com>
2021-03-10 20:49:20 -0800

    clean code

--- getVsPost/index.js
+++ getVsPost/index.js
@@ -3,35 +3,20 @@ const app = express();
 const path = require("path");
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
-// const bodyParser = require("body-parser");
-// const multer = require('multer')
-// const upload = multer()
-// app.use(bodyParser.json());
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-const words = [
-  { username: "gdawg", comment: "lol hey this is me" },
-  { username: "dips", comment: "hey this is Dips" },
-  { username: "shree", comment: "hey yea yea!" },
+const intro = [
+  { username: "gdawg", comment: " Hey this is me Gurkha!" },
+  { username: "dips", comment: "Hey this is Dips" },
+  { username: "shree", comment: "Hey yea! Nice to meet ya" },
 ];
 
 app.get("/comments", (req, res) => {
-  res.render("index", { info: words });
+  res.render("index", { intro });
 });
 
-app.get("/tacos", (req, res) => {
-  //   const { tacos } = req.params;
-  //   res.render("bell", { info: tacos });
-
-  res.send("tacos get request");
-});
-
-app.post("/cars", (req, res) => {
-  const { color, car } = req.body;
-
-  res.send(`your color is ${color} and your car is ${car}`);
-});
 app.listen(3000, (req, res) => {
   console.log("hi");
 });
